adacce394a1eb587f4709d8410bd2e7b295ed90c
Vbarbosa <131600563+yogurlapiz@users.noreply.github.com>
2025-02-03 20:14:18 +0000

    Second Commit

--- exercises/22-The_Beatles/app.js
+++ exercises/22-The_Beatles/app.js
@@ -1,4 +1,18 @@
-
+function sing() {
+    let song = "";
+    for (let string = 1; string <= 11; string++){
+        if (string = 5 ) {
+            console.log("there will be an answer,");
+        }
+        else if (string = 11){
+            console.log("whisper words of wisdom, let it be");
+        }
+        else {
+            console.log("Let it be,");
+        }
+    }
+    return song;
+}
 
 //Your code above ^^^
 
